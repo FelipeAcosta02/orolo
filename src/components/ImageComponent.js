c3ad76ab933c65ce86285e7a5717e4728bf77172
FelipeAcosta02 <jesusfacosta02@gmail.com>
2018-04-05 13:53:46 -0600

    Solved a layout issue

--- src/components/ImageComponent.js
+++ src/components/ImageComponent.js
@@ -3,7 +3,7 @@ import React from 'react';
 const imageComponent = ( props ) => {
     return (
         <div className="ImageComponent">
-            <img src={props.imgSrc} alt="Magnificent Portrait of Orolo our Queen" width="100%" useMap="#leftmap" useMap="#rightmap"/>
+            <img src={props.imgSrc} alt="Magnificent Portrait of Orolo our Queen" width="100%" usemap="#leftmap" usemap="#rightmap"/>
             <map name="leftmap">
                 <area shape="rect" coords="200,120,250,170" href="https://youtu.be/dQw4w9WgXcQ" />
             </map>
