aedea18f409bcd5077e22722541332ec0b015a02
FelipeAcosta02 <jesusfacosta02@gmail.com>
2018-04-06 08:18:31 -0600

    Solved some shit

--- src/components/ImageComponent.js
+++ src/components/ImageComponent.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const imageComponent = ( props ) => {
     return (
-        <div className="ImageComponent">
+        <div style={{marginBottom:'20px'}}>
             <img src={props.imgSrc} alt="Magnificent Portrait of Orolo our Queen" width="100%" usemap="#leftmap" usemap="#rightmap"/>
             <map name="leftmap">
                 <area shape="rect" coords="200,120,250,170" href="https://youtu.be/dQw4w9WgXcQ" />
--- src/containers/App.js
+++ src/containers/App.js
@@ -15,7 +15,7 @@ const carousel = Carousel
 
 class App extends Component {
   state = {
-    qnas: [ {q: "Can you give me a quick tutorial?", a: "Write a question above. Just make sure you include a question mark! (?)"} ],
+    qnas: [ {q: "Can you give me a quick tutorial?", a: "Write a question above. Just make sure you include a question mark!"} ],
     answersNSFW: [ "Never, bitch", "Well, thing is you a hoe", "HAHAHAHA, bitch!", "Pinchi pendejo wey", "I raped you once. You didn't notice.", "Miss me with that gay shit!", "Over my nude body bitch!", "Stop doing this shit Bro!", "You fucking pig!", "What a wicked mind of you, asshole", "I am deeply pondering on why you so fucking dumb", "I don't even exist yet I can see just how fucking dumb u r", "Ur gay. That's not bad. Bad thing is you also fucking stupid", "Would you stop being a hoe? That's the real question!", "I don't answer hoes", "I refuse to answer bitches", "U so cheap bitch", "Okay, I hear u. I hear ur bitchiness", "Your crush doesn't love you. How? You bitch", "ERES UNA GOLFA, G O L F A!" ],
     answearsSFW: [ "You're literallly sexually assaulting me! #MeToo", "I see you at night", "I have been alive for so much time, but I refuse to answer", "Absolutely!", "I didn't know abortion fail rates where so high", "Did you really expect a good answer?! I was made in under 3 hours!", "you should have died long time ago. How old r u?", "You're not asking the real questions", "The Ottomans, they're very OP", "Haha!, I laugh about you", "The fact you keep asking me makes u as worthless as those who created me", "No way!", "Yes", "No", "You wish", "Third grade niggas be like", "u wot m8?!", "Ur mom gay", "... OK! I'm done with you now", "u gay", "I used to be unsure about that as well", "Where did you learn to write?", "I would answer if you weren't a huge dissapointment", "I can't answer that, but I can tell u my gender is gendervoid foxkin demiqueer, and I'm offended", "I may not be answering, but I must say u should leave to be productive", ]
   }
@@ -48,7 +48,7 @@ class App extends Component {
     if (value==="") {
       this.openNotification('This is not a valid question', 'Your question may not be empty!')
     }else if (value.slice(-1)!=="?"){
-      this.openNotification('This is not a valid question','Your question should end with a question mark! (?)')
+      this.openNotification('This is not a valid question','Your question should end with a question mark!')
     }else{
       this.questionInputHandler(value)
     }
